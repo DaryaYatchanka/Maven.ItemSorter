58635adfd5e1bd4381b687dcffd8199fb6f0b039
Darya <Darya.yatchanka@gmail.com>
2019-06-10 13:57:03 -0400

    ItemSorterDone

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+
+  public int compare(Item o1, Item o2) {
+    return Long.compare(o1.getId(), o2.getId());
+            //o1.getId().compareTo(o2.getId());
+//or if else statment with 0 if equals, 1 if greater and -1 if less;
+  }
+
 }
--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,34 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+        return o1.getName().compareTo(o2.getName());
+
+//        public int compareTo(String anotherString){
+//            int len1 = value.length;
+//            int len2 = anotherString.value.length;
+//            int lim = Math.min(len1, len2);
+//            char v1[] = value;
+//            char v2[] = anotherString.value;
+//
+//            int k = 0;
+//            while (k < lim) {
+//                char c1 = v1[k];
+//                char c2 = v2[k];
+//                if (c1 != c2) {
+//                    return c1 - c2;
+//                }
+//                k++;
+//            }
+//            return len1 - len2;
+//        }
+    }
 }
